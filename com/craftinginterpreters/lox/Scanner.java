d4206e5b19885853fcc87b817993ad758405145f
lucasjurgensen <jurgensenlucas@gmail.com>
2022-09-05 14:38:09 -0700

    Chapter 8

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -112,7 +112,7 @@ class Scanner {
         String text = source.substring(start, current);
         TokenType type = keywords.get(text);
         if (type == null) type = IDENTIFIER;
-        addToken(IDENTIFIER);
+        addToken(type);
     }
 
     private void number() {

ae34cf97aea99d32d6e501f353957d515cb6d5f9
lucasjurgensen <jurgensenlucas@gmail.com>
2022-09-20 17:16:39 -0700

    Chapter 10

---
+++ com/craftinginterpreters/lox/Return.java
@@ -0,0 +1,11 @@
+package com.craftinginterpreters.lox;
+
+class Return extends RuntimeException{
+    final Object value;
+
+    Return(Object value) {
+        super(null, null, false, false);
+        this.value = value;
+    }
+}
+
